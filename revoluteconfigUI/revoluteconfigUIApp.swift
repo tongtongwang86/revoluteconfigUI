d3784b30f05ece35babcaab90d5c6396355790ec
Tong tong <t0ngx2@icloud.com>
2024-07-23 22:00:54 +0800

    Initial Commit

---
+++ revoluteconfigUI/revoluteconfigUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  revoluteconfigUIApp.swift
+//  revoluteconfigUI
+//
+//  Created by Tong tong wang on 23/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct revoluteconfigUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ revoluteconfigUIUITests/revoluteconfigUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  revoluteconfigUIUITestsLaunchTests.swift
+//  revoluteconfigUIUITests
+//
+//  Created by Tong tong wang on 23/07/2024.
+//
+
+import XCTest
+
+final class revoluteconfigUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
